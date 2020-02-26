1872407bc486eb745b23943e232acf26574f4835
wanglei <1327578214@qq.com>
2020-02-26 22:35:49 +0800

    Courses

---
+++ GongJiaJuSystem/Models/Courses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GongJiaJuSystem.Models
+{
+    public class Courses
+    {
+        int teacher { get; set; }
+    }
+}
